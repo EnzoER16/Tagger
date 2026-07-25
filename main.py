5b8eccbca1dc45b812aa9d3011b6a8024f88f39d
Enzo Rojas <enzo.e.rojas@gmail.com>
2026-07-25 01:23:32 -0300

    feat: add drag and drop selection

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import flet as ft, os
+import flet as ft, flet_dropzone as ftd, os
 
 def main(page: ft.Page):
     page.title = "Tagger"  
@@ -6,13 +6,33 @@ def main(page: ft.Page):
     page.window.height = 500
     page.padding = 15
 
-    async def select_file(e):
+    async def select_file():
         file = await ft.FilePicker().pick_files(
             dialog_title="Select MP3 file",
             file_type=ft.FilePickerFileType.CUSTOM,
             allowed_extensions=["mp3"])
         if file:
             manage_file(file[0].path)
+
+    def on_drop(event: ftd.DropzoneEvent):
+        files_dropped = event.files
+
+        if len(files_dropped) != 1:
+            file_text.value = "One file allowed at a time"
+            file_text.color = ft.Colors.RED
+            file_text.update()
+            return
+
+        file = files_dropped[0]
+
+        if file.lower().endswith(".mp3"):
+            file_text.value = os.path.basename(file)
+            file_text.color = ft.Colors.WHITE
+        else:
+            file_text.value = "Only mp3 files are allowed"
+            file_text.color = ft.Colors.RED
+
+        file_text.update()
 
     def manage_file(path: str):
         file_text.value = os.path.basename(path)
@@ -27,7 +47,18 @@ def main(page: ft.Page):
         icon=ft.Icons.INSERT_DRIVE_FILE_OUTLINED,
         on_click=select_file)
 
-    page.add(file_text, select_button)
+    main_container = ft.Container(
+        ft.Column(
+            [file_text, select_button]))
+
+    dropzone = ftd.Dropzone(
+        expand=True,
+        on_dropped=on_drop)
+
+    page.add(
+        ft.Stack(
+            [dropzone, main_container],
+            expand=True))
 
 if __name__ == "__main__":
     ft.run(main)
